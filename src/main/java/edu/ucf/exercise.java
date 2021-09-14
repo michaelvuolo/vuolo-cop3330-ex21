a9003ac240fb39139c33ab7c4b04f39d73215861
michaelvuolo <michaelvuolo1@gmail.com>
2021-09-13 21:26:10 -0400

    done

---
+++ src/main/java/edu/ucf/exercise.java
@@ -0,0 +1,56 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Michael Vuolo
+ */
+
+package edu.ucf;
+
+import java.util.Scanner;
+
+public class exercise {
+    public static void main(String[] args) {
+        // create new scanner
+        Scanner input = new Scanner(System.in);
+
+        // prompt for number of month
+        System.out.print("Please enter the number of the month: ");
+        int month = input.nextInt();
+        String monthName = new exercise().convertIntToMonth(month);
+
+        // output month
+        System.out.print("The name of the month is " + monthName + ".");
+    }
+
+    // convert integer to month
+    public String convertIntToMonth(int month) {
+        switch (month) {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+            default:
+                return "unavailable";
+        }
+    }
+}
+
